ca98f1bec8aec12e05d832a066d67785c0036d3a
Bagasaditama13 <bagasjr13@gmail.com>
2026-01-13 19:37:06 +0700

    Ujian Akhir SemesteR

--- src/services/authService.jsx
+++ src/services/authService.jsx
@@ -4,10 +4,7 @@ const API_URL = "https://jwt-auth-eight-neon.vercel.app"; // URL backend
 
 export const loginService = async (email, password) => {
   try {
-    const response = await axios.post(
-      `${API_URL}/login`,
-      { email, password }, 
-    );
+    const response = await axios.post(`${API_URL}/login`, { email, password });
 
     return response.data;
   } catch (error) {
@@ -17,10 +14,35 @@ export const loginService = async (email, password) => {
 
 export const logoutService = async () => {
   try {
-    // Clear token from localStorage
-    localStorage.removeItem("token");
-    return { msg: "Logout berhasil" };
+    const token = localStorage.getItem("token");
+
+    await axios.post(
+      `${API_URL}/logout`,
+      {},
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    );
   } catch (error) {
-    throw error.response?.data || { msg: "Logout gagal" };
+    throw {
+      status: error.response?.status,
+      msg: error.response?.data?.msg,
+    };
+  }
+};
+
+export const registerService = async (name, email, password) => {
+  try {
+    const response = await axios.post(`${API_URL}/register`, {
+      name,
+      email,
+      password,
+    });
+
+    return response.data;
+  } catch (error) {
+    throw error.response?.data || { msg: "Register gagal" };
   }
 };
